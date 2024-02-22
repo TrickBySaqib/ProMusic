1ac15be18664fde0346ffe01523682ae86ed79be
BikashHalder1 <130992701+BikashHalder1@users.noreply.github.com>
2024-02-22 20:00:44 +0530

    Update shayri.py

--- VIPMUSIC/plugins/bot/shayri.py
+++ VIPMUSIC/plugins/bot/shayri.py
@@ -64,9 +64,9 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/Team_Hunter_X"),
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/LineWord122"),
                     InlineKeyboardButton(
-                        "✨𝙲𝙷𝙰𝚃✨", url=f"https://t.me/II_CHAT_HUB_II")
+                        "✨Owner✨", url=f"https://t.me/SaqibADM")
                     
                 ]
             ]
