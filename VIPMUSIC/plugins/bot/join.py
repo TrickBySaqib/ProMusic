8bd9d6fc837d3cd98d9fa00a67115192882debe9
BikashHalder1 <130992701+BikashHalder1@users.noreply.github.com>
2024-02-22 20:03:00 +0530

    Update join.py

--- VIPMUSIC/plugins/bot/join.py
+++ VIPMUSIC/plugins/bot/join.py
@@ -6,7 +6,7 @@ from VIPMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "ll_THE_BAD_BOT_ll"
+MUST_JOIN = "LineWord122"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
